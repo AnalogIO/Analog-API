4049ee4f53e582b0fcb5a48b8678688ddb2782f4
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-02-21 14:55:06 +0100

    Made an ApiClient as a separate component, rather than a set of static methods.

--- Analog-API/Controllers/ShiftsController.cs
+++ Analog-API/Controllers/ShiftsController.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Newtonsoft.Json;
@@ -14,108 +11,46 @@ namespace Analog_API.Controllers
     [Route("api/[controller]")]
     public class ShiftsController : Controller
     {
-        private ApplicationInfo _appInfo;
+        private readonly ShiftplanningApiClient _client;
 
         public ShiftsController()
         {
-            ReadJsonSecrets();
+            _client = new ShiftplanningApiClient(ReadJsonSecrets());
         }
 
-        private void ReadJsonSecrets()
+        private static ApplicationInfo ReadJsonSecrets()
         {
             using (Stream stream = new FileStream("../../SECRETS.json", FileMode.Open))
             {
                 using (var r = new StreamReader(stream))
                 {
                     var json = r.ReadToEnd();
-                    _appInfo = JsonConvert.DeserializeObject<ApplicationInfo>(json);
+                    return JsonConvert.DeserializeObject<ApplicationInfo>(json);
                 }
             }
         }
 
-
         [HttpGet]
-        [Route("~/api/shifts/today")]
+        [Route("today")]
         public async Task<IEnumerable<Shift>> GetToday()
         {
-            return (await Get()).Where(shift => shift.Open.Date == DateTime.Now.Date);
+            return await _client.GetShifts(DateTime.Today, DateTime.Today);
         }
-
 
         // GET: api/shifts
         [HttpGet]
         public async Task<IEnumerable<Shift>> Get()
         {
-            var loggedIn = await FetchData.Login(_appInfo);
+            return await _client.GetShifts();
+        }
 
-            if (loggedIn.Token != null)
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
             {
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
-
-                var request = new ShiftsRequest
-                {
-                    Module = "schedule.shifts",
-                    Method = "GET",
-                    StartDate = DateTime.Now,
-                    EndDate = DateTime.Now.AddMonths(12),
-                    Mode = ShiftsRequest.OverviewMode
-                };
-
-                loggedIn.Request = request;
-
-                var dict = new Dictionary<string, string>
-                {
-                    ["data"] = JsonConvert.SerializeObject(loggedIn)
-                };
-
-                var content = new FormUrlEncodedContent(dict);
-
-                var result = await client.PostAsync("https://www.shiftplanning.com/api/", content);
-
-                if (result.IsSuccessStatusCode)
-                {
-                    result.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
-                    var str = await result.Content.ReadAsStringAsync();
-
-                    var res = JsonConvert.DeserializeObject<ApiResponse<IEnumerable<ShiftsData>>>(str);
-
-                    return
-                        res?.Data.Select(
-                            shift => new Shift
-                            {
-                                Open = DateTime.Parse(shift.start_timestamp),
-                                Close = DateTime.Parse(shift.end_timestamp),
-                                EmployeeNames = shift.employees.Select(emp => emp.name.Split(' ').FirstOrDefault())
-                            });
-                }
+                _client.Dispose();
             }
-            return new Shift[0];
-        }
-
-        // GET api/shift/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/shifts
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/shifts/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/shifts/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
