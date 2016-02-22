edcac3444092f8e16a5f55aab7d7acabf583f070
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-02-22 13:26:06 +0100

    Added another controller.

---
+++ Analog-API/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Analog_API.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Analog_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly IShiftplanningApiClient _client;
+
+        public EmployeesController(IConfiguration config)
+        {
+            var appinfo = config.Get<ApplicationInfo>("secrets");
+
+            _client = new ShiftplanningApiClient(appinfo);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var result = await _client.GetEmployee(id);
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+            return Ok(result);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                _client.Dispose();
+            }
+        }
+    }
+}
