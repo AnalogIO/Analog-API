84ee342e129463e2e01a299846e360e3610f0471
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-02-24 09:33:10 +0100

    Added old Analog-website as root site.

---
+++ Analog-API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using TamigoApiClient.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Analog_API.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+
+            var shifts = JsonConvert.DeserializeObject<IEnumerable<Shift>>(await client.GetStringAsync("http://localhost:43655/api/shifts"));
+
+            return View(shifts);
+        }
+    }
+}
