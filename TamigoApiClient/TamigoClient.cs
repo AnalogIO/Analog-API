6f71f6223513ebd8721766d954f26fb9f7f66f92
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-02-29 16:12:34 +0100

    Try to fix wrong order of shifts.

--- TamigoApiClient/TamigoClient.cs
+++ TamigoApiClient/TamigoClient.cs
@@ -122,7 +122,7 @@ namespace TamigoApiClient
                         Open = grouping.Key,
                         Close = grouping.First().EndTime,
                         Employees = grouping.Select(shift => shift.EmployeeName.Split(' ').First())
-                    });
+                    }).OrderBy(shift => shift.Open);
             }
             else
             {
